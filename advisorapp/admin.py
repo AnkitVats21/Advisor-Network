c23f21d467248bc326953d535521acbe56c55707
AnkitVats21 <ankvats21@gmail.com>
2021-05-08 11:40:18 +0530

    custom user model created

--- advisorapp/admin.py
+++ advisorapp/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from .models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ('email', 'admin', 'active')
+    list_filter = ('admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('active', 'admin','staff')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
